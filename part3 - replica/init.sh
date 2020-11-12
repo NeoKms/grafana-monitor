7e7446d3ea724316032d22f5b09c963e963584db
Jiuglskiy.Vladislav <Jiuglskiy.Vladislav@chtp.net>
2020-11-12 22:38:02 +0300

    add part 3 - replica

---
+++ part3 - replica/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker swarm init && docker service create --name registry --publish published=5000,target=5000 registry:2
+docker-compose build && docker-compose push
+docker stack deploy --compose-file docker-compose.yml stackdemo && docker stack services stackdemo
